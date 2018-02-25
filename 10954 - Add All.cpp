154a81db1bcd3a266702020b0fe18e7c264c1164
AEhab <ahmedehabdarwich@gmail.com>
2018-02-25 20:23:32 +0200

    Create 10954 - Add All.cpp

---
+++ 10954 - Add All.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //freopen("in.in","r",stdin);
+    //freopen("out.out","w",stdout);
+    while(1)
+    {
+        multimap <int,int>mp;
+        int n,cost=0;
+        cin >> n;
+        if(!n)break;
+        priority_queue <int> pq1;
+        for(int i=0; i<n; i++)
+        {
+            int x;
+            cin>>x;
+            pq1.push(x*-1);
+        }
+        int j=0;
+        while(pq1.size()>1)
+        {
+            int sum = pq1.top();
+            pq1.pop();
+            sum+=pq1.top();
+            pq1.pop();
+            cost+=sum;
+            pq1.push(sum);
+        }
+        cout <<cost*-1<<endl;
+    }
+    return 0;
+}
