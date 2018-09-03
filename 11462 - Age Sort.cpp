2e1e9295abab710e8dbdf7f9607de14b621acee6
Ahmed Ehab Darwich <ahmedehabdarwich@gmail.com>
2018-09-03 02:33:02 +0200

    Add files via upload

---
+++ 11462 - Age Sort.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Author      : Ahmed Ehab
+//============================================================================
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	//freopen("in.in", "r", stdin);
+	//freopen("out.out", "w", stdout);
+	while (1) {
+		long long n;
+		scanf("%lld", &n);
+		vector<long long> vec(n);
+		if (!n)
+			break;
+		for (int i = 0; i < n; i++) {
+			scanf("%lld", &vec[i]);
+		}
+		sort(vec.begin(), vec.end());
+		for (int i = 0; i < n; i++) {
+			if (i != 0)
+				printf(" ");
+			printf("%lld", vec[i]);
+		}
+		printf("\n");
+	}
+	return 0;
+
+}
+
---
+++ 441 - Lotto.cpp
@@ -0,0 +1,46 @@
+//============================================================================
+// Author      : Ahmed Ehab
+//============================================================================
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	//freopen("in.in", "r", stdin);
+	//freopen("out.out", "w", stdout);
+	int k;
+	bool f=0;
+	while (1) {
+		cin >> k;
+		if (!k)
+			break;
+		if(f)
+			cout << endl;
+		f=1;
+		int arr[k];
+
+		for (int i = 0; i < k; i++)
+			cin >> arr[i];
+		for (int i = 0; i < k; i++) {
+			for (int j = i+1; j < k; j++) {
+				for (int ii = j+1; ii < k; ii++) {
+					for (int jj = ii+1; jj < k; jj++) {
+						for (int iii = jj+1; iii < k; iii++) {
+							for (int jjj = iii+1; jjj < k; jjj++) {
+								cout << arr[i]<< " "<< arr[j] <<" ";
+								cout  << arr[ii]<< " "<< arr[jj] <<" ";
+								cout  << arr[iii]<< " "<< arr[jjj] <<"\n";
+							}
+						}
+
+					}
+				}
+
+			}
+		}
+	}
+
+	return 0;
+}
+
